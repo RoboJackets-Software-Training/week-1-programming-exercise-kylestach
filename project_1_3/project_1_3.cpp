b89a9283b21b022b81a2c266e1c4e12b635ab6d7
Kyle Stachowicz <kylestach99@gmail.com>
2020-09-16 16:57:35 -0400

    Complete

--- project_1_3/project_1_3.cpp
+++ project_1_3/project_1_3.cpp
@@ -35,13 +35,44 @@ int main() {
   std::cin >> s;
   w = readInVector(s);
 
-  // TODO write your code here
-  // =========== START =========
 
+  std::cout << "x: {" << x[0];
+  for(int i = 1; i < x.size(); i++) {
+    std::cout << ", " << x[i];
+  }
+  std::cout << "}" << std::endl;
 
+  std::cout << "w: {" << w[0];
+  for(int i = 1; i < w.size(); i++) {
+    std::cout << ", " << w[i];
+  }
+  std::cout << "}" << std::endl;
 
+  int packing_size = (w.size()-1)/2;
 
-  // =========== END ===========
+  for(int i = 0; i < x.size(); i++) {
+    double accumulator = 0;
+    for(int j = 0; j < w.size(); j++) {
+
+      // Since kernel is filled in with 0's outside does not matter
+      if(i - packing_size + j >= 0 && i - packing_size + j < x.size()) {
+        accumulator += x[i - packing_size + j]*w[j];
+      } else if(!pack_with_zeros && i - packing_size + j < 0) {
+        // Handles the case where where we underflow indexes
+        accumulator += x[0] * w[j];
+      } else if(!pack_with_zeros && i - packing_size + j >= x.size()) {
+        // Handles the case where we overflow indexes
+        accumulator += x[x.size()-1] * w[j];
+      }
+    }
+    y.push_back(accumulator);
+  }
+
+  std::cout << "{" << y[0];
+  for(int i = 1; i < y.size(); i++) {
+    std::cout << ", " << y[i];
+  }
+  std::cout << "}" << std::endl;
 
   return 0;
 }
